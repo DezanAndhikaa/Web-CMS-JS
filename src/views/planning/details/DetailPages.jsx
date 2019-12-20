5b67d6abe118018b01b41d7362373d52b7802e3d
IRFAN FAHTHONI -PARTNER CST <cst_dev113@unitedtractors.com>
2019-12-20 15:57:52 +0700

    #12654 - FE - Membuat fungsi input free text numeric

--- src/views/planning/details/DetailPages.jsx
+++ src/views/planning/details/DetailPages.jsx
@@ -27,7 +27,6 @@ class DetailPages extends React.Component{
     componentDidMount(){
         console.log("narik data sales order ")
         console.log(this.state.lifetime)
-        
     }
     
     _renderPagination() {
