89737bf96d813b93a40e0846deb42e595ca27616
IRFAN FAHTHONI -PARTNER CST <cst_dev113@unitedtractors.com>
2020-01-06 16:40:23 +0700

    fixing input lifetime data from API

--- src/views/planning/details/components/PlanningList/SalesOrderList.jsx
+++ src/views/planning/details/components/PlanningList/SalesOrderList.jsx
@@ -20,8 +20,9 @@ export default class SalesOrderList extends React.PureComponent {
     //   this.props.storeJobData(row);
     //   return this.props.pushTo(`${Menu.DETAIL_PI}:${row.woNumber || ''}`);
     // }
-    componentDidMount = async() =>{
-      await this.props.onClickSalesOrder();
+
+    componentDidMount = () =>{
+       this.props.onClickSalesOrder();
     }
 
     isCheckboxAvailable = (data) => {
@@ -114,9 +115,8 @@ render(){
           </TableRow>
         </TableHead>
         <TableBody classes={{ root: 'table-body' }}>
-          {this.props.salesOrderList.Lists
-            && this.props.salesOrderList.Lists.map((row, id) => (
-              // {/* {SalesOrderData.salesData && SalesOrderData.salesData.map((row, id) => ( */}
+          {this.props.value
+            && this.props.value.map((row, id) => (
               <TableRow key={id} classes={{ root: 'table-row' }}>
                 <TableCell padding="checkbox">
                   {this.props.displayCheckbox && <Checkbox disabled={this.isCheckboxAvailable(row)} checked={this.props.selectedSalesPlanList.selectedSales.some((plans) => plans.SO === row.SO)} onClick={() => this.props.onChoosedSales(row)} classes={{ checked: 'checkbox-checked' }} />}
@@ -130,8 +130,8 @@ render(){
                 <TableCell align="left" className="table-cell"> {row.UnitCode} </TableCell>
                 <TableCell align="left" className="table-cell"> {row.SerialNumber} </TableCell>
                 <TableCell align="center" className="table-cell"> 
-                {!this.props.salesOrderList.Lists[id].LifeTimeComp ? <InputButton title="Input Lifetime Component" onStats={this.props.onStats} titles="Input" key={row.SO} id={row.SO}/> : 
-                  <div>{this.props.salesOrderList.Lists[id].LifeTimeComp}</div>
+                {!this.props.value[id].LifeTimeComp ? <InputButton title="Input Lifetime Component" onStats={this.props.onStats} titles="Input" key={row.SO} id={row.SO}/> : 
+                  <div>{this.props.value[id].LifeTimeComp}</div>
                 }
                 </TableCell>
                 <TableCell align="left" className="table-cell"> {row.PlanExecution} </TableCell>
