0a9c53be68e3de99c480701cfb0e97fb16beaf80
anbiyasr <cst_dev114@unitedtractors.com>
2020-01-29 13:28:08 +0700

    develop list data in tracking history page

--- src/views/planning/details/components/PlanningList/TrackingHistory/TrackingHistory.jsx
+++ src/views/planning/details/components/PlanningList/TrackingHistory/TrackingHistory.jsx
@@ -1,23 +1,112 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import Cards from './components/Card';
-import PlanningDetailsTab from '../../Tab/PlanningDetailsTab';
+import SearchInput from '../../../../../../components/Searchbar/SearchInput';
+import BaseButton from '../../../../../../components/Button/BaseButton';
+import SalesOrderList from '../SalesOrderList';
+import ApprovedSalesOrderList from '../ApprovedSalesOrderList';
 import '../../../../../../components/FilterByTitle/DropdownFilter.scss';
 import '../../../../details/components/Tab/PlanningDetailsTab.scss';
 import '../../../DetailPages.scss';
 
 export default class TrackingHistory extends React.PureComponent {
-	
-	_renderTabs(){
-		return(
-			<>
-				<PlanningDetailsTab 
-					{...this.props}
-				/>
-			</>
-		);
+	state ={
+		whatPageIsChoosed : ''
 	}
 
+	_renderSearchBar(){
+		return (
+		  <div className="bottom-row">
+			<SearchInput
+			{...this.props}
+			webInfo="Search"
+			onSalesSearch={this.props.onSearchSales}
+			onServiceSearch={this.props.onSearchService}
+		  />
+		  </div>
+		);
+	  }
+	_renderDownloadBtn(){
+		return(
+			<BaseButton titles="Download"
+            {...this.props}
+            // handleSalesApprovedDownload={this.handleSalesApprovedDownload}
+            // selectedDownloadData={this.state.selectedData.So} 
+          />
+		)
+	}
+	onClickSalesOrder = () =>{
+		this.props.fetchSalesOrder(this.props.salesParameter.dataFilter);
+	  }
+	_renderSalesOrderList(){
+		return(
+		  <div className="plannings-list-containers">
+			<SalesOrderList 
+			{...this.props}
+			displaySalesCheckbox={this.props.salesParameter.paramsData.assigmentFilter || this.props.salesParameter.paramsData.inProgressFilter}
+			sortSalesByState={this.props.sortSalesBy}
+			onClickSalesOrder={this.onClickSalesOrder}
+			onChoosedSales={this.updateAssignmentSalesStates}
+			selectedSalesPlanList={this.props.selectedSalesPlans}
+			/>
+		  </div>
+		);
+	  }
+
+
+	  _renderApprovedSalesOrderList(){
+		return(
+		  <div className="plannings-list-containers">
+			<ApprovedSalesOrderList 
+			{...this.props}
+			displaySalesCheckbox={this.props.salesParameter.paramsData.assigmentFilter || this.props.salesParameter.paramsData.inProgressFilter}
+			sortSalesByState={this.props.sortSalesBy}
+			onClickSalesOrder={this.onClickSalesOrder}
+			onChoosedSales={this.updateAssignmentSalesStates}
+			selectedSalesPlanList={this.props.selectedSalesPlans}
+			/>
+		  </div>
+		);
+	  }
+	//   componentDidUpdate = () => {
+	// 	this.refresh()
+	//   }
+	  refresh(){
+		window.location.reload(false);
+	  }
+
+	  _renderList = (whatPageIsChoosed) =>{
+		// const whatPageIsChoosed = '';
+		this.setState({
+			whatPageIsChoosed : whatPageIsChoosed
+		})
+		switch (this.state.whatPageIsChoosed) {
+			case 'Approve':
+				console.log('this is approved', whatPageIsChoosed)
+				return(
+					this._renderApprovedSalesOrderList()
+				)
+			case 'Not Approve': 
+			console.log('this is not approved', whatPageIsChoosed)
+				return (
+					this._renderSalesOrderList()
+				)
+			default:
+				console.log('this is default', whatPageIsChoosed)
+				return(
+					this._renderApprovedSalesOrderList()
+				)
+		}
+	  }
+
+	  updateAssignmentSalesStates = (plan) => {
+		if (this.props.selectedSalesPlans
+		  .some((plans) => plans.So === plan.So,
+			// console.log('sssss sales', this.state.selectedData.So)
+		  )) 
+		{ return this.props.unselectSalesPlan(plan); }
+		return this.props.selectSalesPlan(plan);
+	  };
 	componentDidMount = () =>{
 		this.props.fetchSalesOrder(this.props.salesParameter.dataFilter);
 		this.props.fetchServiceOrder(this.props.serviceParameter.dataFilter);
@@ -34,24 +123,26 @@ export default class TrackingHistory extends React.PureComponent {
 				<div className="table-container">
 					<div className="filters-container">
 						<div className="dropdowns-container">
-						<Cards title="Approve" totalData={this.props.salesOrderListApproved.TotalData} /> &nbsp; 
-						<Cards title="Not Approve" totalData={this.props.salesOrderList.TotalData}/> &nbsp; 
-						<Cards title="Delete" totalData={this.props.salesOrderListDeleted.TotalData} /> &nbsp; 
-						<Cards title="SAP ISSUE" totalData="0" />
+						<Cards title="Approve" totalData={this.props.salesOrderListApproved.TotalData} renderList={this._renderList} /> &nbsp; 
+						<Cards title="Not Approve" totalData={this.props.salesOrderList.TotalData} renderList={this._renderList} /> &nbsp; 
+						<Cards title="Delete" totalData={this.props.salesOrderListDeleted.TotalData} renderList={this._renderList} /> &nbsp; 
+						<Cards title="SAP ISSUE" totalData="0" renderList={this._renderList} />
 						</div>
-					</div>
-					{/* {this._renderTabs()} */}
-					{/* {this._renderCards()} &nbsp; {this._renderCards()} &nbsp; {this._renderCards()} &nbsp; {this._renderCards()}
-					 */}
+						 <p1> TRACKING HISTORY - Sales Order </p1> 
+						 <br />
+						<div className="search-container">							
+        					{this._renderSearchBar()}
+        				</div>
+						<br />
+						<div className="base-button-container">
+							{this._renderDownloadBtn()}
+						</div>
+						</div>
+				</div>
+				<div>
+				{this._renderList(this.state.whatPageIsChoosed)}
 				</div>
 			</main>
-			// <div className="filters-container">
-			// 	<div className="dropdowns-container">
-			// 		<div className="dropdown-container">
-			// 			<Cards /> &nbsp; <Cards /> &nbsp; <Cards /> &nbsp; <Cards />
-			// 		</div>					
-			// 	</div>
-			// </div>
 			
 			
 			
--- src/views/planning/details/components/PlanningList/TrackingHistory/components/Card.jsx
+++ src/views/planning/details/components/PlanningList/TrackingHistory/components/Card.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import { makeStyles, withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
@@ -7,40 +8,22 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import './Card.scss';
 
-const styles  = theme => ({
-	card: {
-		maxWidth: 275,
-		minWidth: 275,
-		marginTop: 170,
-		marginRight: 25,
-	},
-	bullet: {
-		display: 'inline-block',
-		margin: '0 2px',
-		transform: 'scale(0.8)',
-	},
-	title: {
-		fontSize: 14,
-	},
-	pos: {
-		marginBottom: 12,
-	},
-});
 
 class Cards extends React.Component {
 	// const classes = useStyles();
 	
 	// const totalData = props.title;
 	// console.log('kon....t',totalData);
-	handleClick (){
-		alert('yeah')
+	handleClick = () =>{
+		this.props.renderList(this.props.title);
 	}
 
 	render(){
-		const { classes } = this.props;
 		return (
-			<Card classes={{ root : classes.card}}>
+			// <Card classes={{ root : classes.card}}>
+			<Card className="card">
 				<CardActionArea onClick={this.handleClick}>
 					<CardContent>
 						<Typography variant="h5" component="h2">
@@ -51,15 +34,13 @@ class Cards extends React.Component {
 						</Typography>
 					</CardContent>
 					<CardActions>
-						<Button size="small">Learn More</Button>
+						<Button size="small" >Learn More</Button>
 					</CardActions>
 				</CardActionArea>
 			</Card>
 		);
 	}
 }
-Cards.propTypes = {
-	classes: PropTypes.object.isRequired,
-};
 
-export default withStyles(styles, { withTheme: true })(Cards);
+
+export default Cards;
