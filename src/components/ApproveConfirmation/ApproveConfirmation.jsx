257771e32ca0be29bd2c2a080877dce38855e310
DIAN RAHMAWATI - PARTNER CST <cst_dev119@unitedtractors.com>
2020-02-20 10:29:04 +0700

    Merge with 12163-anbiya

--- src/components/ApproveConfirmation/ApproveConfirmation.jsx
+++ src/components/ApproveConfirmation/ApproveConfirmation.jsx
@@ -15,13 +15,13 @@ export default class ApproveConfirmation extends React.PureComponent {
 						<CloseNotif onClose={this.props.onClose}/>
 						<div className="confirmation-container">
 							<p className="confirmation-title">Approve Planning</p>
-							<p className="confirmation-title">From Service Order</p>
+							<p className="confirmation-title">From Sales Order</p>
 							<img className="confirmation-image" src={Confirm} alt="" />
 							<p className="confirmation-caption">Are you sure want to</p>
 							<p className="confirmation-caption">approve {this.props.totalData} items?</p>
 							<div className="btn-row">
-								<Button className="button-no" onClick={this.props.onClose}>No</Button> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-								<Button className="button-yes" onClick={this.props.onApprove}>Yes</Button>
+								<Button className="button-rejected" onClick={this.props.onClose}>No</Button> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+								<Button className="button-approved" onClick={this.props.onApprove}>Yes</Button>
 							</div>
 						</div>
 					</div>
