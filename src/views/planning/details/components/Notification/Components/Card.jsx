5a1659c92f4eb95a660d451ff34bc3ab5ba7be4d
DIAN RAHMAWATI - PARTNER CST <cst_dev119@unitedtractors.com>
2020-02-20 16:49:20 +0700

    #13199 FE - Membuat komponen notifikasi

--- src/views/planning/details/components/Notification/Components/Card.jsx
+++ src/views/planning/details/components/Notification/Components/Card.jsx
@@ -3,45 +3,72 @@ import { Card } from '@material-ui/core'
 
 export default class CardData extends React.Component {
 
-    constructor(props) {
-        super(props)
-        this.state = {
-          col: [
-            {},
-            {},
-            {},{},{}
-          ],
-          events: [
-            { id: 1, time: "22 agustus 2018 - 9:00", title: "Jembayan", location: "Telah Menginput Lifetime Component", push: "dengan SO : 138580736 dan 13751973" },
-            { id: 2, time: "22 agustus 2018 - 9:00", title: "Jembayan", location: "Telah Menginput Lifetime Component", push: "dengan SO : 138580733 dan 13151973" },
-            { id: 3, time: "22 agustus 2018 - 9:00", title: "Jembayan", location: "Telah Menginput Lifetime Component", push: "dengan SO : 138580731 dan 13451973" },
-          ]
-          // nextPage: true,
-          // prevPage: false,
-          // numberOfPage: 2,
-          // currentPage: 1,
-          // filter: {
-          //   filter : {}
-          // }
-        };
-    }
-    render(){
-        return(
+  constructor(props) {
+    super(props)
+    this.state = {
+      col: [
+        {},
+        {},
+        {},{},{}
+      ],
+      events: [
+        { id: 1, time: "22 agustus 2018 - 9:00", title: "Jembayan", location: "Telah Menginput Lifetime Component", push: "dengan SO : 138580736 dan 13751973" },
+        { id: 2, time: "22 agustus 2018 - 9:00", title: "Jembayan", location: "Telah Menginput Lifetime Component", push: "dengan SO : 138580733 dan 13151973" },
+        { id: 3, time: "22 agustus 2018 - 9:00", title: "Jembayan", location: "Telah Menginput Lifetime Component", push: "dengan SO : 138580731 dan 13451973" },
+      ],
+      notif: [{}],
+      list: [
+        { id: 1, time: "22 Agustus 2018 - 10:00", title: "Jembayan", location: "Telah Menginput Lifetime Component", push: "dengan SO : 138580736 dan 13751973" },
+        { id: 2, time: "20 September 2018 - 9:00", title: "Jakarta", location: "Telah Menginput Lifetime Component", push: "dengan SO : 138580733 dan 13151973" },
+        { id: 3, time: "12 Oktober 2018 - 11:00", title: "Bandung", location: "Telah Menginput Lifetime Component", push: "dengan SO : 138580731 dan 13451973" },
+        { id: 4, time: "22 Oktober 2018 - 8:00", title: "Surabaya", location: "Telah Menginput Lifetime Component", push: "dengan SO : 138580733 dan 13151973" },
+        { id: 5, time: "28 November 2018 - 9:40", title: "Bekasi", location: "Telah Menginput Lifetime Component", push: "dengan SO : 138580731 dan 13451973" },
+      ]
+      // nextPage: true,
+      // prevPage: false,
+      // numberOfPage: 2,
+      // currentPage: 1,
+      // filter: {
+      //   filter : {}
+      // }
+    };
+  }
+  render(){
+    if(this.props.idCard === "Notification"){
+      return(
+        <>
+          {this.state.notif.map(() => (
             <>
-            {this.state.col.map(() => (
-                <div className="filters-container">
-                  {this.state.events.map(world => (
-                       <Card className="kartu">
-                         <div className="kartu-title">{world.time}</div>
-                         <div className="kartu-site">{world.title}</div>
-                         <div className="kartu-title">{world.location}</div>
-                         <div className="kartu-title">{world.push}</div>
-                       </Card>
-                       
-                  ))}
-                </div>
-           ))}
-           </>
-        )
+              {this.state.list.map(world => (
+                <Card className="list-item-notif">
+                  <div className="kartu-judul">{world.time}</div>
+                  <div className="kartu-nama-site">{world.title}</div>
+                  <div className="kartu-judul">{world.location}</div>
+                  <div className="kartu-judul">{world.push}</div>
+                </Card>
+              ))}
+            </>
+          ))}
+        </>
+      )
+    } 
+    else if(this.props.idCard === "See All"){
+      return(
+          <>
+          {this.state.col.map(() => (
+            <div className="filters-container">
+              {this.state.events.map(world => (
+                <Card className="kartu">
+                  <div className="kartu-title">{world.time}</div>
+                  <div className="kartu-site">{world.title}</div>
+                  <div className="kartu-title">{world.location}</div>
+                  <div className="kartu-title">{world.push}</div>
+                </Card>
+              ))}
+            </div>
+          ))}
+          </>
+      )
     }
+  }
 }
