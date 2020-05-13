51b2d069e272a252cc039601761f6c1fc74fa462
DIAN RAHMAWATI - PARTNER CST <cst_dev119@unitedtractors.com>
2020-05-13 13:10:37 +0700

    #18298 FE - Membuat role login CMS

---
+++ src/utils/roleService.helper.js
@@ -0,0 +1,29 @@
+import jwtDecode from 'jwt-decode';
+import { clientIdData } from './userLocalService';
+
+export default class roleService {
+  constructor() {
+    this.jwtDecode = null;
+  }
+
+  role = () => {
+    try {
+      this.jwtDecode = jwtDecode(clientIdData().accessToken);
+      const role_name = this.jwtDecode.role_name;
+      let role = null;
+      if ((Array.isArray(role_name) && role_name.includes('OBD')) || role_name === 'OBD') {
+        role = 1;
+      }else if ((Array.isArray(role_name) && role_name.includes('SDH')) || role_name === 'SDH') {
+        role = 2;
+      }else if ((Array.isArray(role_name) && role_name.includes('PDC')) || role_name === 'PDC') {
+        role = 3;
+      }else if ((Array.isArray(role_name) && role_name.includes('SPV')) || role_name === 'SPV') {
+        role = 4;
+      }
+      return role;
+    } catch (error) {
+      this.jwtDecode = error;
+      return this.jwtDecode;
+    }
+  };
+}
---
+++ src/utils/userLocalService.helper.js
@@ -0,0 +1,32 @@
+const INITIAL_DATA = {
+  // You can put here data from localStorage like name, profile etc
+  accessToken: null,
+};
+
+// For get data information from PCAR Login
+export const clientIdData = () => {
+  const clientId = localStorage.getItem(process.env.REACT_APP_CLIENT_ID);
+
+  if (clientId) {
+    return {
+      ...INITIAL_DATA,
+      accessToken: JSON.parse(clientId).tokenResponse.accessToken,
+    };
+  }
+  return { ...INITIAL_DATA };
+};
+
+// For get data information from Portal Login
+export const userData = () => {
+  const user = localStorage.getItem('userData');
+
+  if (user) {
+    return {
+      // You can put here data from localStorage like name, profile etc
+      accessToken: JSON.parse(user).tokenResponse.accessToken,
+    };
+  }
+  return {
+    accessToken: null,
+  };
+};
