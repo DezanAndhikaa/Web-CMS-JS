8efbe27b75d7cec4530c3390a8ae855a45ba6f96
DIAN RAHMAWATI - PARTNER CST <cst_dev119@unitedtractors.com>
2020-08-31 23:30:51 +0700

    #22432 FE - Revisi komponen search bar

--- src/views/planning/details/components/Status/Status.jsx
+++ src/views/planning/details/components/Status/Status.jsx
@@ -1401,10 +1401,11 @@ export default class Status extends React.PureComponent {
 		return (
 		  <div className="bottom-rows">
 			<SearchInput
-			{...this.props}
-			webInfo="Search"
-			handleSearch={this.handleSearch}
-		  />
+				{...this.props}
+				idTab= "Status"
+				webInfo="Search"
+				handleSearch={this.handleSearch}
+			/>
 		  </div>
 		);
 	}
