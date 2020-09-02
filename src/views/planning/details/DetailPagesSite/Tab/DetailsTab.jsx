6ca1f70f6d02e6aed4793537c19a414920c8debe
DIAN RAHMAWATI - PARTNER CST <cst_dev119@unitedtractors.com>
2020-09-02 15:50:46 +0700

    #22421 FE - Testing fungsi

--- src/views/planning/details/DetailPagesSite/Tab/DetailsTab.jsx
+++ src/views/planning/details/DetailPagesSite/Tab/DetailsTab.jsx
@@ -129,8 +129,11 @@ class DetailsTab extends React.Component {
   _renderSalesOrderList(){
     return(
       <>
-      <div className={this.props.salesOrderList.Lists.length === 0 
-          && this.props.fetchStatusSales === ApiRequestActionsStatus.SUCCEEDED ? "list-detail-empty" : "plannings-list-detail"}>
+      <div 
+        className={this.props.salesOrderList.Lists.length === 0 
+        && this.props.fetchStatusSales === ApiRequestActionsStatus.SUCCEEDED ? 
+        "list-detail-empty" : "plannings-list-detail"}
+      >
         <SalesOrderList 
           {...this.props}
           pageLoc= "Status"
