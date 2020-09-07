547cf2b226a60666c457d3212f8ccabd27ae085e
DIAN RAHMAWATI - PARTNER CST <cst_dev119@unitedtractors.com>
2020-09-07 13:52:41 +0700

    #22542 FE - Styling tabel pop up SAP Issue

--- src/views/planning/details/components/SapIssue/SapIssue.jsx
+++ src/views/planning/details/components/SapIssue/SapIssue.jsx
@@ -22,21 +22,21 @@ export default class SapIssue extends React.Component{
       <TableHead className="table-head-issue" >
         <TableRow className="table-row-issue">
           {this.props.whichTabs ? 
-            <TableCell align="left" className="table-cell">SO</TableCell> : 
-            <TableCell align="left" className="table-cell">WO</TableCell>
+            <TableCell align="left" className="tc-pk-head">SO</TableCell> : 
+            <TableCell align="left" className="tc-pk-head">WO</TableCell>
           }
-          <TableCell align="left" className="table-cell">CUSTOMER</TableCell>
-          <TableCell align="left" className="table-cell">SITE</TableCell>
-          <TableCell align="left" className="table-cell">UNIT MODEL</TableCell>
-          <TableCell align="left" className="table-cell">COMPONENT DESCRIPTION</TableCell>
-          <TableCell align="left" className="table-cell">PART NUMBER</TableCell>
-          <TableCell align="left" className="table-cell">UNIT CODE</TableCell>
-          <TableCell align="left" className="table-cell">SERIAL NUMBER</TableCell>
-          <TableCell align="left" className="table-cell">LIFETIME COMP</TableCell>
-          <TableCell align="left" className="table-cell">PLAN EXECUTION</TableCell>
-          <TableCell align="left" className="table-cell">SMR </TableCell>
-          <TableCell align="left" className="table-cell">SMR DATE</TableCell>
-          <TableCell align="left" className="table-cell">PLAN TYPE</TableCell>
+          <TableCell align="left" className="table-cell-issue">CUSTOMER</TableCell>
+          <TableCell align="left" className="table-cell-issue">SITE</TableCell>
+          <TableCell align="left" className="table-cell-issue">UNIT MODEL</TableCell>
+          <TableCell align="left" className="table-cell-issue">COMPONENT DESC</TableCell>
+          <TableCell align="left" className="table-cell-issue">PART NUMBER</TableCell>
+          <TableCell align="left" className="table-cell-issue">UNIT CODE</TableCell>
+          <TableCell align="left" className="table-cell-issue">SERIAL NUMBER</TableCell>
+          <TableCell align="left" className="table-cell-issue">LIFETIME COMP</TableCell>
+          <TableCell align="left" className="table-cell-issue">PLAN EXECUTION</TableCell>
+          <TableCell align="left" className="table-cell-issue">SMR </TableCell>
+          <TableCell align="left" className="table-cell-issue">SMR DATE</TableCell>
+          <TableCell align="left" className="table-cell-issue">PLAN TYPE</TableCell>
         </TableRow>
       </TableHead>
     )
@@ -44,32 +44,32 @@ export default class SapIssue extends React.Component{
 
   _showTableBody(row, id) {
     return (
-      <>
-        <TableRow className="table-row-issue">
+      <TableBody className= "table-body-issue">
+        <TableRow className="table-row-top-issue">
           {this.props.whichTabs ?
-            <TableCell align="left" className="table-cell-issue"> {row.SoNumber} </TableCell> : 
-            <TableCell align="left" className="table-cell-issue"> {row.WoNumber} </TableCell>
+            <TableCell align="left" className="tc-pk-body"> {row.SoNumber} </TableCell> : 
+            <TableCell align="left" className="tc-pk-body"> {row.WoNumber} </TableCell>
           }
-          <TableCell align="left" className="table-cell-issue"> {row.CustomerName} </TableCell>
-          <TableCell align="left" className="table-cell-issue"> {row.SiteCode} </TableCell>
+          <TableCell align="left" className="tc-cst"> {row.CustomerName} </TableCell>
+          <TableCell align="left" className="tc-short"> {row.SiteCode} </TableCell>
           <TableCell align="left" className="table-cell-issue"> {row.UnitModel} </TableCell>
-          <TableCell align="left" className="table-cell-issue"> {row.ComponentDescription} </TableCell>
+          <TableCell align="left" className="tc-long"> {row.ComponentDescription} </TableCell>
           <TableCell align="left" className="table-cell-issue"> {row.PartNumber} </TableCell>
           <TableCell align="left" className="table-cell-issue"> {row.UnitCode} </TableCell>
           <TableCell align="left" className="table-cell-issue"> {row.SerialNumber} </TableCell>
           <TableCell align="left" className="table-cell-issue"> {row.LifeTimeComponent}</TableCell>
           <TableCell align="left" className="table-cell-issue"> {moment(row.PlanExecution).format('DD-MM-YYYY')} </TableCell>
-          <TableCell align="left" className="table-cell-issue"> {row.SMR} </TableCell>
+          <TableCell align="left" className="tc-smr"> {row.SMR} </TableCell>
           <TableCell align="left" className="table-cell-issue"> {moment(row.SMRDate).format('DD-MM-YYYY')} </TableCell>
           <Tooltip arrow title={row.PlanType.charAt(0) === "U" ? "UNSCHEDULE" : ""} >
-            <TableCell align="left" className="table-cell"> {row.PlanType.substring(0, 3)} </TableCell>
+            <TableCell align="left" className="table-cell-issue"> {row.PlanType.substring(0, 3)} </TableCell>
           </Tooltip>
         </TableRow>
         <TableRow className="table-row-bottom-issue">
           <TableCell colSpan="1"><label>Description<label className="label-required">*</label></label></TableCell>
           <TableCell colSpan="12">{this._showDescription(id)}</TableCell>
         </TableRow>
-      </>
+      </TableBody>
     )
   }
 
@@ -77,14 +77,14 @@ export default class SapIssue extends React.Component{
     return(
       <div className="teks">
         <TextField 
-            className="teks"
-            type='text' 
-            placeholder="Tuliskan Masalahnya yaa.."
-            variant="outlined" 
-            size="small"
-            value={this.state.description[id]} 
-            name={this.state.description[id]} 
-            onChange={this.handleChange.bind(this, id)} 
+          className="teks"
+          type='text' 
+          placeholder="Tuliskan Masalahnya yaa.."
+          variant="outlined" 
+          size="small"
+          value={this.state.description[id]} 
+          name={this.state.description[id]} 
+          onChange={this.handleChange.bind(this, id)} 
         />
       </div>
     )
@@ -102,12 +102,10 @@ export default class SapIssue extends React.Component{
           </div>
           <div className="top-middle-issue"> 
             <Table size="small" component={Paper}>
-                {this._showTableHead()}
-                <TableBody>
-                  {this.props.selectedDataSAP
-                      && this.props.selectedDataSAP.map((row, id) => (this._showTableBody(row,id)) )
-                  }
-                </TableBody>
+              {this._showTableHead()}
+              {this.props.selectedDataSAP
+                && this.props.selectedDataSAP.map((row, id) => (this._showTableBody(row,id)) )
+              }
             </Table>
           </div>
           <div className="bottom-row-issue">
@@ -123,9 +121,9 @@ export default class SapIssue extends React.Component{
     return(
       <div>
         <Modal className="modal-pos-issue" open={this.props.isShowModal} onClose={this.props.isClosed} >
-            <div className="body-container">
-                {this._renderIssue()}
-            </div>
+          <div className="body-container">
+            {this._renderIssue()}
+          </div>
         </Modal>
       </div>
     )
