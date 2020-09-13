b9c8c7dcd8daf41b5376eb8f962608068f13a551
DIAN RAHMAWATI - PARTNER CST <cst_dev119@unitedtractors.com>
2020-09-13 20:37:11 +0700

    #22902 FE - Revisi fungsi pagination (tabel revisi)

--- src/views/planning/details/DetailPagesSite/DetailPagesSite.jsx
+++ src/views/planning/details/DetailPagesSite/DetailPagesSite.jsx
@@ -571,7 +571,7 @@ componentDidUpdate = (prevProps) => {
     if (this.state.whichTabs === true) {
       const web = this.props.displayMode === 'web';
       const currentPropsRev = this.props.salesOrderRevised.PageNumber;
-      const { TotalPages } = this.props.salesOrderRevised;
+      const { TotalPages } = this.props.salesOrderRevised.Lists;
       
       return(
         <div className="paginations-rev">
