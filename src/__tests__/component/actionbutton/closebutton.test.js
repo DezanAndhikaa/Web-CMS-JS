06fcf09b3fe51956227b91eab82d37a71db5bcf8
DIAN RAHMAWATI - PARTNER CST <cst_dev119@unitedtractors.com>
2020-09-22 11:55:21 +0700

    #23037 FE - Unit testing action button - close button

---
+++ src/__tests__/component/actionbutton/closebutton.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Enzyme, { shallow } from "enzyme";
+import Adapter from 'enzyme-adapter-react-16';
+import CloseButton from "../../../components/ActionButton/CloseButton/CloseButton";
+
+Enzyme.configure({adapter:new Adapter()});
+it("render closebutton correctly", () => {
+    const wrapper = shallow (
+      <CloseButton />
+    )
+    expect(wrapper).toMatchSnapshot();
+
+})
