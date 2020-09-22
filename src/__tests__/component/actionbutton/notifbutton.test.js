a82514ec5c9c60caa5ff53b2ffb079708661eab5
DIAN RAHMAWATI - PARTNER CST <cst_dev119@unitedtractors.com>
2020-09-22 12:05:43 +0700

    #23041 FE - Unit testing notif button

---
+++ src/__tests__/component/actionbutton/notifbutton.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Enzyme, { shallow } from "enzyme";
+import Adapter from 'enzyme-adapter-react-16';
+import NotifButton from "../../../components/ActionButton/NotifButton/NotifButton";
+
+Enzyme.configure({adapter:new Adapter()});
+it("render notif button correctly", () => {
+    const wrapper = shallow (
+      <NotifButton />
+    )
+    expect(wrapper).toMatchSnapshot();
+
+})
