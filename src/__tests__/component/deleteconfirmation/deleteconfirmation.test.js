dcd958ef0c3f3174bde95f6b523ea369064f20a5
DIAN RAHMAWATI - PARTNER CST <cst_dev119@unitedtractors.com>
2020-09-23 06:41:42 +0700

    #23049 FE - Unit testing delete confirmation

---
+++ src/__tests__/component/deleteconfirmation/deleteconfirmation.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Enzyme, { shallow } from "enzyme";
+import Adapter from 'enzyme-adapter-react-16';
+import DeleteConfirmation from '../../../components/DeleteConfirmation/DeleteConfirmation';
+
+Enzyme.configure({adapter:new Adapter()});
+it("render button close notif correctly", () => {
+    const wrapper = shallow (
+      <DeleteConfirmation />
+    )
+    expect(wrapper).toMatchSnapshot();
+
+})
