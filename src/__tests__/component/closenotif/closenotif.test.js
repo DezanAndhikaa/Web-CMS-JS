39f2ed83dc663ae348ba3c2d301f3c5c34f677ca
DIAN RAHMAWATI - PARTNER CST <cst_dev119@unitedtractors.com>
2020-09-22 12:43:51 +0700

    #23047 FE - Unit testing close notif

---
+++ src/__tests__/component/closenotif/closenotif.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Enzyme, { shallow } from "enzyme";
+import Adapter from 'enzyme-adapter-react-16';
+import CloseNotif from '../../../components/CloseNotif/CloseNotif';
+
+Enzyme.configure({adapter:new Adapter()});
+it("render button close notif correctly", () => {
+    const wrapper = shallow (
+      <CloseNotif />
+    )
+    expect(wrapper).toMatchSnapshot();
+
+})
