d5e56ce8817bc5e1f3e21610ce2e7f5d5ed5e045
DIAN RAHMAWATI - PARTNER CST <cst_dev119@unitedtractors.com>
2020-09-23 07:09:48 +0700

    #23050 FE - Unit testing dropdown list

---
+++ src/__tests__/component/dropdownlist/dropdownlist.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Enzyme, { shallow } from "enzyme";
+import Adapter from 'enzyme-adapter-react-16';
+import DropdownList from '../../../components/DropdownList/DropDownList';
+
+Enzyme.configure({adapter:new Adapter()});
+it("render dropdown list correctly", () => {
+    const wrapper = shallow (
+        <DropdownList />
+    )
+    expect(wrapper).toMatchSnapshot();
+})
