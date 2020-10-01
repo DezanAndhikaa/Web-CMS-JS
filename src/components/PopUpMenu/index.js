5273d8b1308c476bc0f8c62f770c8d3a6fb941f6
DIAN RAHMAWATI - PARTNER CST <cst_dev119@unitedtractors.com>
2020-10-01 22:20:29 +0700

    #23266 FE - Penerapan absolute path pop up menu

--- src/components/PopUpMenu/index.js
+++ src/components/PopUpMenu/index.js
@@ -1,9 +1,9 @@
 import { connect } from 'react-redux';
 import { push } from 'connected-react-router';
 import PopUpMenu from './PopUpMenu';
-import { removeDataAction } from '../../core/StorageHelper';
+import { removeDataAction } from 'core/StorageHelper';
 import { USER_DATA } from '../../constants';
-import { logoutAction } from '../../actions/Planning';
+import { logoutAction } from 'actions/Planning';
 
 const mapStateToProps = (state) => ({
 	path: state.router.location.pathname,
