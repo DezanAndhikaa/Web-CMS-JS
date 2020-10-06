05bc585ccdde45598dda8ab937525de667b462ae
DIAN RAHMAWATI - PARTNER CST <cst_dev119@unitedtractors.com>
2020-10-06 14:38:24 +0700

    #23453 FE - Absolute path (unit testing filter by lifetime)

--- __tests__/component/filterbylifetime/filterbylifetime.test.js
+++ __tests__/component/filterbylifetime/filterbylifetime.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Enzyme, { shallow } from "enzyme";
 import Adapter from 'enzyme-adapter-react-16';
-import FilterByLifetime from '../../../src/components/FilterByLifetime/FilterByLifetime';
+import FilterByLifetime from 'components/FilterByLifetime/FilterByLifetime';
 
 Enzyme.configure({adapter:new Adapter()});
 it("render filter by lifetime correctly", () => {
