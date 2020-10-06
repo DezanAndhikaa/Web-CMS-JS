3c189011c752ea3a08617cca13f2010152e1b76f
DIAN RAHMAWATI - PARTNER CST <cst_dev119@unitedtractors.com>
2020-10-06 14:41:55 +0700

    #23458 FE - Absolute path (unit testing message)

--- __tests__/component/message/message.test.js
+++ __tests__/component/message/message.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Enzyme, { shallow } from "enzyme";
 import Adapter from 'enzyme-adapter-react-16';
-import Message from '../../../src/components/Message/Message';
+import Message from 'components/Message/Message';
 
 Enzyme.configure({adapter:new Adapter()});
 it("render error message component correctly", () => {
