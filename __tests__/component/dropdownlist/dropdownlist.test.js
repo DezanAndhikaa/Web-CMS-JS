4810bb64152e0783229a9cf5e84a2cdf8bc83b53
DIAN RAHMAWATI - PARTNER CST <cst_dev119@unitedtractors.com>
2020-10-06 14:35:37 +0700

    #23450 FE - Absolute path (unit testing dropdown list)

--- __tests__/component/dropdownlist/dropdownlist.test.js
+++ __tests__/component/dropdownlist/dropdownlist.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Enzyme, { shallow } from "enzyme";
 import Adapter from 'enzyme-adapter-react-16';
-import DropdownList from '../../../src/components/DropdownList/DropDownList';
+import DropdownList from 'components/DropdownList/DropDownList';
 
 Enzyme.configure({adapter:new Adapter()});
 it("render dropdown list correctly", () => {
