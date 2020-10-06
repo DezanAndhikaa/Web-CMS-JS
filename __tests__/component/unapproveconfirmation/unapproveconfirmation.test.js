57f7fbbb81acd659f4f4e92f1ed0777e01bc15f2
DIAN RAHMAWATI - PARTNER CST <cst_dev119@unitedtractors.com>
2020-10-06 14:50:21 +0700

    #23463 FE - Absolute path (unit testing modal unapprove)

--- __tests__/component/unapproveconfirmation/unapproveconfirmation.test.js
+++ __tests__/component/unapproveconfirmation/unapproveconfirmation.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Enzyme, { shallow } from "enzyme";
 import Adapter from 'enzyme-adapter-react-16';
-import UnapproveConfirmation from '../../../src/components/UnapproveConfirmation/UnapproveConfirmation';
+import UnapproveConfirmation from 'components/UnapproveConfirmation/UnapproveConfirmation';
 
 Enzyme.configure({adapter:new Adapter()});
 it("render modal unapprove confirmation correctly", () => {
