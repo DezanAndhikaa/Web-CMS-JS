6c72757a8a7e7d91198f359f6dd7e8f9ac7d8876
DIAN RAHMAWATI - PARTNER CST <cst_dev119@unitedtractors.com>
2020-10-06 14:32:47 +0700

    #23446 FE - Absolute path (unit testing close notif)

--- __tests__/component/closenotif/closenotif.test.js
+++ __tests__/component/closenotif/closenotif.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Enzyme, { shallow } from "enzyme";
 import Adapter from 'enzyme-adapter-react-16';
-import CloseNotif from '../../../src/components/CloseNotif/CloseNotif';
+import CloseNotif from 'components/CloseNotif/CloseNotif';
 
 Enzyme.configure({adapter:new Adapter()});
 it("render button close notif correctly", () => {
