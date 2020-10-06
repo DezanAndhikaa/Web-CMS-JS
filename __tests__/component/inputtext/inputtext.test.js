c701a408c2db94df436e75be849174ee5f152180
DIAN RAHMAWATI - PARTNER CST <cst_dev119@unitedtractors.com>
2020-10-06 14:44:41 +0700

    #23457 FE - Absolute path (unit testing input text)

--- __tests__/component/inputtext/inputtext.test.js
+++ __tests__/component/inputtext/inputtext.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Enzyme, { shallow } from "enzyme";
 import Adapter from 'enzyme-adapter-react-16';
-import InputText from '../../../src/components/InputText/InputText';
+import InputText from 'components/InputText/InputText';
 
 Enzyme.configure({adapter:new Adapter()});
 it("render input text correctly", () => {
