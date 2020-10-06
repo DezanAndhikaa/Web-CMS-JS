dd8e9371662ab518a20f31e65c67b10ff94dee42
DIAN RAHMAWATI - PARTNER CST <cst_dev119@unitedtractors.com>
2020-10-06 14:47:38 +0700

    #23461 FE - Absolute path (unit testing search bar)

--- __tests__/component/searchbar/searchbar.test.js
+++ __tests__/component/searchbar/searchbar.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Enzyme, { shallow } from "enzyme";
 import Adapter from 'enzyme-adapter-react-16';
-import SearchInput from "../../../src/components/Searchbar/SearchInput";
+import SearchInput from "components/Searchbar/SearchInput";
 
 Enzyme.configure({adapter:new Adapter()});
 it("render searchbar correctly", () => {
