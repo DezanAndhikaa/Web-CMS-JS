e5c4fe9745487e4bf311490e895fe8dcba26c102
DIAN RAHMAWATI - PARTNER CST <cst_dev119@unitedtractors.com>
2020-10-06 14:31:09 +0700

    #23444 FE - Absolute path (unit testing button)

--- __tests__/component/button/basebutton.test.js
+++ __tests__/component/button/basebutton.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Enzyme, { shallow } from "enzyme";
 import Adapter from 'enzyme-adapter-react-16';
-import BaseButton from "../../../src/components/Button/BaseButton";
+import BaseButton from "components/Button/BaseButton";
 
 Enzyme.configure({adapter:new Adapter()});
 it("render base button correctly", () => {
--- __tests__/component/button/inputbutton.test.js
+++ __tests__/component/button/inputbutton.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Enzyme, { shallow } from "enzyme";
 import Adapter from 'enzyme-adapter-react-16';
-import InputButton from "../../../src/components/Button/InputButton";
+import InputButton from "components/Button/InputButton";
 
 Enzyme.configure({adapter:new Adapter()});
 it("render input button correctly", () => {
