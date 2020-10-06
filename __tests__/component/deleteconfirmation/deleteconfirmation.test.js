4cd7174e88752cd772de4c622f0ec5b85cdfa5b8
DIAN RAHMAWATI - PARTNER CST <cst_dev119@unitedtractors.com>
2020-10-06 14:34:33 +0700

    #23449 FE - Absolute path (unit testing modal delete)

--- __tests__/component/deleteconfirmation/deleteconfirmation.test.js
+++ __tests__/component/deleteconfirmation/deleteconfirmation.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Enzyme, { shallow } from "enzyme";
 import Adapter from 'enzyme-adapter-react-16';
-import DeleteConfirmation from '../../../src/components/DeleteConfirmation/DeleteConfirmation';
+import DeleteConfirmation from 'components/DeleteConfirmation/DeleteConfirmation';
 
 Enzyme.configure({adapter:new Adapter()});
 it("render button close notif correctly", () => {
