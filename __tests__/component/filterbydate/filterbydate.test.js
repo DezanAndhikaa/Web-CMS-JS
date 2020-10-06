71c1401b596401ea0485c2088f52691384d5b627
DIAN RAHMAWATI - PARTNER CST <cst_dev119@unitedtractors.com>
2020-10-06 14:39:06 +0700

    #23454 FE - Absolute path (unit testing filter by date)

--- __tests__/component/filterbydate/filterbydate.test.js
+++ __tests__/component/filterbydate/filterbydate.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Enzyme, { shallow } from "enzyme";
 import Adapter from 'enzyme-adapter-react-16';
-import FilterByPeriodDate from '../../../src/components/FilterByPeriodeDate/FilterByPeriodeDate';
+import FilterByPeriodDate from 'components/FilterByPeriodeDate/FilterByPeriodeDate';
 
 Enzyme.configure({adapter:new Adapter()});
 it("render filter by period date correctly", () => {
