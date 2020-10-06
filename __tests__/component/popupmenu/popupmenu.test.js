e384bd0fc361aef6cd37aa0b0d07e22dd5115101
DIAN RAHMAWATI - PARTNER CST <cst_dev119@unitedtractors.com>
2020-10-06 14:48:33 +0700

    #23460 FE - Absolute path (unit testing pop up menu)

--- __tests__/component/popupmenu/popupmenu.test.js
+++ __tests__/component/popupmenu/popupmenu.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Enzyme, { shallow } from "enzyme";
 import Adapter from 'enzyme-adapter-react-16';
-import PopUpMenu from '../../../src/components/PopUpMenu/PopUpMenu';
+import PopUpMenu from 'components/PopUpMenu/PopUpMenu';
 
 Enzyme.configure({adapter:new Adapter()});
 it("render pop up menu correctly", () => {
